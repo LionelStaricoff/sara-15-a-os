324d4f8527aa00e36c9a877c3fe95fc6c631e278
lionel staricoff <staricofflionel@hotmail.com>
2024-08-23 23:34:13 -0500

    creando funcion crearSpanAnimation

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import {Util} from './js/Util.js'
+import {crearSpanAnimation} from './CrearSpanAnimation.js'
+
 document.addEventListener("scroll", () => { Util.scrollFx(); });
 
+new crearSpanAnimation(null, 'pedro  pablo', null)
 
