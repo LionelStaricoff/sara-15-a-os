e9e6630275d7744ade14a856d8185e81a709dd2d
lionel staricoff <staricofflionel@hotmail.com>
2024-08-26 18:29:36 -0500

    agregando funcion click al button cupon

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 import { Util } from './js/Util.js'
 
-
-document.addEventListener("scroll", () => { Util.scrollFx(); });
-
-
-Util.crearSectionCuatro();
-// Actualizar contador cada segundo
-setInterval(Util.mostrarContador, 1000);
+try {
+    document.addEventListener("scroll", () => { Util.scrollFx(); });
 
 
- Util.crearSectionQuinto();
+    Util.crearSectionCuatro();
+    // Actualizar contador cada segundo
+    setInterval(Util.mostrarContador, 1000);
 
- Util.crearSectionSeis();
+
+    Util.crearSectionQuinto();
+
+    Util.crearSectionSeis();
+
+    const cupo = document.querySelector('#cupo');
+    cupo.addEventListener('click', ()=>{
+        alert('cupos')
+    })
+
+} catch (error) {
+    console.error(error)
+}
+
 
