26816bfff241d7451030469d6ef3d63792c4346d
lionel staricoff <staricofflionel@hotmail.com>
2024-08-27 00:51:56 -0500

    creando los 4 formularios

--- js/formulario.js
+++ js/formulario.js
@@ -12,13 +12,50 @@ export class Formulario {
         this._x = document.createElement('button');
         this._x.innerText = "X";
         this._x.addEventListener('click', () => this._padre.removeChild(this._fondo));
-        this.crearCss()
+
     }
 
     unaPersona() {
+        this.crearCss();
         const input = document.createElement('input');
         input.placeholder = 'Nombre completo'
         this._fondo.append(this._x, input, this._button)
+        this._padre.appendChild(this._fondo)
+    }
+
+    dosPersonas() {
+        this.crearCss2();
+        const input = document.createElement('input');
+        input.placeholder = 'Nombre completo';
+        const input2 = document.createElement('input');
+        input2.placeholder = 'Nombre completo'
+        this._fondo.append(this._x, input, input2, this._button)
+        this._padre.appendChild(this._fondo)
+    }
+
+    tresPersonas() {
+        this.crearCss3();
+        const input = document.createElement('input');
+        input.placeholder = 'Nombre completo';
+        const input2 = document.createElement('input');
+        input2.placeholder = 'Nombre completo';
+        const input3 = document.createElement('input');
+        input3.placeholder = 'Nombre completo';
+        this._fondo.append(this._x, input, input2, input3, this._button)
+        this._padre.appendChild(this._fondo)
+    }
+
+    cuatroPersonas() {
+        this.crearCss4();
+        const input = document.createElement('input');
+        input.placeholder = 'Nombre completo';
+        const input2 = document.createElement('input');
+        input2.placeholder = 'Nombre completo';
+        const input3 = document.createElement('input');
+        input3.placeholder = 'Nombre completo';
+        const input4 = document.createElement('input');
+        input4.placeholder = 'Nombre completo';
+        this._fondo.append(this._x, input, input2, input3, input4, this._button)
         this._padre.appendChild(this._fondo)
     }
 
@@ -101,5 +138,200 @@ border-radius: 25px;
         head.appendChild(style);
     }
 
+    crearCss2() {
+        const head = document.head;
+        const style = document.createElement('style');
+        style.innerText = `
+        #formulario {
+    position: fixed;
+    z-index: 2;
+    width: min(20em, 100%);
+    height: min(30em, 100%);
+border-radius: 25px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    background-image: url('../img/1/fondo_formulario.webp');
+
+    display: grid;
+    justify-content: space-around;
+    text-align: center;
+    place-items: center;
+    gap: 20px;
+    opacity: 0.9;
+    border: solid 3px #333;
+    
+
+    button:nth-child(1) {
+        height: fit-content;
+        width: fit-content;
+        font-size: 2.2em;
+        font-weight: bolder;
+        position: relative;
+        transform: translate(100px, 0px);
+        background-color: inherit;
+        color: rgb(211, 21, 21);
+        border: none;
+
+        &:hover {
+            font-weight: 200;
+        }
+    }
+
+    input:nth-child(2), input:nth-child(3) {
+        text-align: center;
+        border-radius: 15px;
+        padding: 0.5em;
+        font-size: 1.2em;
+    }
+
+    button:nth-child(4) {
+        height: fit-content;
+        width: fit-content;
+        background-color: rgb(51, 51, 51, 20%);
+        color: whitesmoke;
+        padding: 0.5em;
+        font-size: 1.5em;
+        border-radius: 15px;
+
+    }
+}
+        `
+
+        head.appendChild(style);
+    }
+
+    crearCss3() {
+        const head = document.head;
+        const style = document.createElement('style');
+        style.innerText = `
+     #formulario {
+    position: fixed;
+    z-index: 2;
+    width: min(20em, 100%);
+    height: min(30em, 100%);
+border-radius: 25px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    background-image: url('../img/1/fondo_formulario.webp');
+
+    display: grid;
+    justify-content: space-around;
+    text-align: center;
+    place-items: center;
+    gap: 20px;
+    opacity: 0.9;
+    border: solid 3px #333;
+    
+
+    button:nth-child(1) {
+        height: fit-content;
+        width: fit-content;
+        font-size: 2.2em;
+        font-weight: bolder;
+        position: relative;
+        transform: translate(100px, 0px);
+        background-color: inherit;
+        color: rgb(211, 21, 21);
+        border: none;
+
+        &:hover {
+            font-weight: 200;
+        }
+    }
+
+    input:nth-child(2), input:nth-child(3), input:nth-child(4) {
+        text-align: center;
+        border-radius: 15px;
+        padding: 0.5em;
+        font-size: 1.2em;
+    }
+
+    button:nth-child(5) {
+        height: fit-content;
+        width: fit-content;
+        background-color: rgb(51, 51, 51, 20%);
+        color: whitesmoke;
+        padding: 0.5em;
+        font-size: 1.5em;
+        border-radius: 15px;
+
+    }
+}
+      
+
+        `
+
+        head.appendChild(style);
+    }
+
+    crearCss4() {
+        const head = document.head;
+        const style = document.createElement('style');
+        style.innerText = `
+     #formulario {
+    position: fixed;
+    z-index: 2;
+    width: min(20em, 100%);
+    height: min(30em, 100%);
+border-radius: 25px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    background-image: url('../img/1/fondo_formulario.webp');
+
+    display: grid;
+    justify-content: space-around;
+    text-align: center;
+    place-items: center;
+    gap: 20px;
+    opacity: 0.9;
+    border: solid 3px #333;
+    
+
+    button:nth-child(1) {
+        height: fit-content;
+        width: fit-content;
+        font-size: 2.2em;
+        font-weight: bolder;
+        position: relative;
+        transform: translate(100px, 0px);
+        background-color: inherit;
+        color: rgb(211, 21, 21);
+        border: none;
+
+        &:hover {
+            font-weight: 200;
+        }
+    }
+
+    input:nth-child(2), input:nth-child(3), input:nth-child(4), input:nth-child(5) {
+        text-align: center;
+        border-radius: 15px;
+        padding: 0.5em;
+        font-size: 1.2em;
+    }
+
+    button:nth-child(6) {
+        height: fit-content;
+        width: fit-content;
+        background-color: rgb(51, 51, 51, 20%);
+        color: whitesmoke;
+        padding: 0.5em;
+        font-size: 1.5em;
+        border-radius: 15px;
+
+    }
+}
+      
+
+        `
+
+        head.appendChild(style);
+    }
 
 }
--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import { Util } from './js/Util.js'
-import {Formulario} from './js/formulario.js';
+import { Formulario } from './js/formulario.js';
 
 
 
 try {
     document.addEventListener("scroll", () => { Util.scrollFx(); });
-
+   // const f1 = new Formulario(null, '573113840944');
+   // f1.unaPersona();
 
     Util.crearSectionCuatro();
     // Actualizar contador cada segundo
@@ -18,8 +19,8 @@ try {
 
     const cupo = document.querySelector('#cupo');
     cupo.addEventListener('click', () => {
-          const f1 = new Formulario(null,'573113840944');
-   f1.unaPersona();
+        const f1 = new Formulario(null, '573113840944');
+        f1.unaPersona();
     })
 
 } catch (error) {
